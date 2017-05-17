ee2516c642c2d678e75ed50b7ea0c35166a7a722
ecneladis <ecneladis@gmail.com>
2017-05-17 08:40:30 +0200

    Update data

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     $('#book-notes').DataTable( {
-"createdRow": function ( row, data, index ) {
+        "createdRow": function ( row, data, index ) {
         },
+        "order": [[ 1, "asc" ]],
         "columns": [
             { "data": "author" },
             { "data": "title" },
