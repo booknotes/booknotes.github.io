31eacf3b4ea65168aa1834151e916b3b84d7ccb7
ecneladis <ecneladis@gmail.com>
2017-05-21 14:57:13 +0200

    Clean design

--- js/main.js
+++ js/main.js
@@ -8,6 +8,12 @@ $(document).ready(function() {
             { "data": "title" },
             { "data": "goodreads_rating" },
             { "data": "review_author" },
-        ]
+        ],
+        "lengthMenu": [[10, 50, 100, -1], [10, 50, 100, "All"]],
+        "dom": '<"top"f>rt<"bottom"lp><"clear">',
+        "language": {
+          "search": "_INPUT_",
+          "searchPlaceholder": "Search books"
+        }
     } );
 } );
